4c42e6e00c6b1b772df8b074aca9aee8bfc13e15
ruggerifrancesco <lavoro.francescoruggeri@gmail.com>
2023-07-24 16:29:22 +0200

    added new route to see comics list

--- app/Http/Controllers/PageController.php
+++ app/Http/Controllers/PageController.php
@@ -9,4 +9,8 @@ class PageController extends Controller
     public function home () {
         return view('home');
     }
+
+    public function indexComics () {
+        return view('comics-index');
+    }
 }
