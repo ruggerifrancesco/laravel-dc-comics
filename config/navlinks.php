2fb6ff86e68aba08491796929aaf9dfe2f35dcd1
ruggerifrancesco <lavoro.francescoruggeri@gmail.com>
2023-07-24 20:58:28 +0200

    updated navlinks (current standard pages)

--- config/navlinks.php
+++ config/navlinks.php
@@ -3,53 +3,12 @@
 return [
     [
         'active' => false,
-        'item' => 'Characters',
+        'item' => 'Home',
         'href' => '#',
     ],
     [
         'active' => true,
-        'item' => 'Comics',
-        'href' => '#',
-    ],
-    [
-        'active' => false,
-        'item' => 'Movies',
-        'href' => '#',
-    ],
-    [
-        'active' => false,
-        'item' => 'Tv',
-        'href' => '#',
-    ],
-    [
-        'active' => false,
-        'item' => 'Games',
-        'href' => '#',
-    ],
-    [
-        'id' => 6,
-        'active' => false,
-        'item' => 'Collectibles',
-        'href' => '#',
-    ],
-    [
-        'active' => false,
-        'item' => 'Videos',
-        'href' => '#',
-    ],
-    [
-        'active' => false,
-        'item' => 'Fans',
-        'href' => '#',
-    ],
-    [
-        'active' => false,
-        'item' => 'News',
-        'href' => '#',
-    ],
-    [
-        'active' => false,
-        'item' => 'Shop',
+        'item' => 'Comics Display',
         'href' => '#',
     ],
 ];
