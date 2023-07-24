fb4217a0a617aaa26821d3d963a86a99098fcf25
ruggerifrancesco <lavoro.francescoruggeri@gmail.com>
2023-07-24 21:02:27 +0200

    added favicon.ico to website

--- resources/views/layout/app.blade.php
+++ resources/views/layout/app.blade.php
@@ -5,6 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>@yield('title', 'Laravel App')</title>
+    <link rel="shortcut icon" href="{{ Vite::asset('resources/img/favicon.ico') }}" type="image/x-icon">
 
     @vite('resources/js/app.js')
 </head>
