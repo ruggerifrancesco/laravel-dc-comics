5e68633d97e6aa7eae67c457e763ef5a2bc362d4
ruggerifrancesco <lavoro.francescoruggeri@gmail.com>
2023-07-24 20:26:30 +0200

    current series comics in new index

--- resources/views/guest/index.blade.php
+++ resources/views/guest/index.blade.php
@@ -3,7 +3,31 @@
 @section('title', 'Comics Index')
 
 @section('main-content')
-    <h1>
-        Comics List Route
-    </h1>
+    <div class="wrapper wrapper-main">
+        <div class="series-section-title">
+            <h2>Current Series</h2>
+        </div>
+
+        <div class="series-wrapper">
+
+            <!-- Comics Item -->
+            {{-- @foreach ($comics as $comic) --}}
+                {{-- <article class="card-item"> --}}
+                    {{-- <div class="card-img"> --}}
+                        {{-- <a href="#"> --}}
+                            {{-- <img src="{{ $comic['thumb'] }}" alt="{{ $comic['series'] }} Series"> --}}
+                        {{-- </a> --}}
+                    {{-- </div> --}}
+
+                    {{-- <div class="card-infos"> --}}
+                        {{-- <h5>{{ $comic['series'] }}</h5> --}}
+                    {{-- </div> --}}
+                {{-- </article> --}}
+            {{-- @endforeach --}}
+        </div>
+
+        <button type="button" class="btn btn-primary" id="load-more-btn">
+            Load more
+        </button>
+    </div>
 @endsection
