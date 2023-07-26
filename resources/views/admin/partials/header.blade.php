4ab359c8587d75054af013b7dec1b3d91de188ce
ruggerifrancesco <lavoro.francescoruggeri@gmail.com>
2023-07-26 19:27:33 +0200

    readded custom route deleted by accident, added new nav link for admin panel

--- resources/views/admin/partials/header.blade.php
+++ resources/views/admin/partials/header.blade.php
@@ -4,10 +4,20 @@
             <div class="admin_page-logo">
                 <img src="{{ Vite::asset('resources/img/dc-logo.png') }}" alt="DC Main Logo">
             </div>
-            <span>Admin Panel</span>
+            <span>Admin Client</span>
         </div>
         <nav>
             <ul class="my_list-nav">
+                <li>
+                    <a href="{{ route('admin.comics.create') }}">
+                        Control Panel
+                    </a>
+                </li>
+                <li>
+                    <a href="{{ route('admin.comics.index') }}">
+                        Comics List
+                    </a>
+                </li>
                 <li>
                     <a href="{{ route('admin.comics.create') }}">
                         <i class="fa-solid fa-plus"></i>
