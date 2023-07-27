e5e0d618f8230dbc0e75cb69518135ad3c641ecf
ruggerifrancesco <lavoro.francescoruggeri@gmail.com>
2023-07-27 10:00:10 +0200

    rechanged single comic

--- resources/views/admin/comic/show.blade.php
+++ resources/views/admin/comic/show.blade.php
@@ -9,49 +9,53 @@
     <div class="card border-warning text-bg-dark mb-3">
         <div class="card-header d-flex justify-content-between">
             <span>
-                Serie: {{ $comics->series }}
+                Serie: {{ $comic->series }}
             </span>
             <span>
-                Type: {{ $comics->type }}
+                Type: {{ $comic->type }}
             </span>
         </div>
         <div class="row g-0">
-          <div class="col-md-5 thumb-container">
-            <img src="{{ $comics->thumb }}" class="img-fluid" alt="{{ $comics->title }}">
-          </div>
-          <div class="col-md-7">
-            <div class="card-body">
-              <h5 class="card-title mb-5">{{ $comics->title }}</h5>
-              <p class="card-text">{{ $comics->description }}</p>
-
-              <div class="accordion accordion-flush" id="accordionFlush">
-                <div class="accordion-item text-bg-warning">
-                  <h2 class="accordion-header" id="flush-headingOne">
-                    <button class="accordion-button text-bg-warning collapsed" type="button"
-                        data-bs-toggle="collapse" data-bs-target="#flush-collapseOne" aria-expanded="false" aria-controls="flush-collapseOne">
-                      More Info
-                    </button>
-                  </h2>
-                  <div id="flush-collapseOne" class="accordion-collapse collapse" aria-labelledby="flush-headingOne" data-bs-parent="#accordionFlush">
-                    <div class="accordion-body">
-                        <ul>
-                            <li>Price: {{ $comics->price }}</li>
-                            <li>Sale Date: {{ $comics->sale_date }}</li>
-                        </ul>
-                    </div>
-                  </div>
-                </div>
-              </div>
+            <div class="col-md-5 thumb-container">
+                <img src="{{ $comic->thumb }}" class="img-fluid" alt="{{ $comic->title }}">
             </div>
-          </div>
+            <div class="col-md-7">
+                <div class="card-body">
+                    <h5 class="card-title mb-5">{{ $comic->title }}</h5>
+                    <p class="card-text">{{ $comic->description }}</p>
+
+                    <div class="accordion accordion-flush" id="accordionFlush">
+                        <div class="accordion-item text-bg-warning">
+                            <h2 class="accordion-header" id="flush-headingOne">
+                                <button class="accordion-button text-bg-warning collapsed" type="button"
+                                    data-bs-toggle="collapse" data-bs-target="#flush-collapseOne" aria-expanded="false" aria-controls="flush-collapseOne">
+                                    More Info
+                                </button>
+                            </h2>
+                            <div id="flush-collapseOne" class="accordion-collapse collapse" aria-labelledby="flush-headingOne" data-bs-parent="#accordionFlush">
+                                <div class="accordion-body">
+                                    <ul>
+                                        <li>Price: {{ $comic->price }}</li>
+                                        <li>Sale Date: {{ $comic->sale_date }}</li>
+                                    </ul>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
         <div class="card-footer d-flex gap-2 justify-content-end">
-            <a href="{{ route('admin.comics.edit', ['comic' => $comics->id]) }}" class="btn btn-info">
+            <a href="{{ route('admin.comics.edit', ['comic' => $comic->id]) }}" class="btn btn-info">
                 <i class="fa-solid fa-pen"></i>
             </a>
-            <a type="button" class="btn btn-danger">
-                <i class="fa-solid fa-trash"></i>
-            </a>
+            <form action="{{ route('admin.comics.destroy', $comic->id) }}" method="post">
+                @csrf
+                @method('DELETE')
+                <button type="submit" class="btn btn-danger">
+                    <i class="fa-solid fa-trash"></i>
+                </button>
+            </form>
         </div>
       </div>
 </main>
